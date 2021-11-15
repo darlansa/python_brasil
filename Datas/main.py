0ee4b95597f3ad9671c2312876d6804ba78df495
Darlan Sá <darlansaa@gmail.com>
2021-11-15 17:50:15 -0300

    criando manipulação de datas e excluindo algumas pastas

---
+++ Datas/main.py
@@ -0,0 +1,8 @@
+from datas_br import DatasBr
+
+cadastro = DatasBr()
+
+print(cadastro.mes_cadastro().capitalize())
+print(cadastro.dia_cadastro().capitalize())
+print(cadastro.tempo_cadatro())
+print(cadastro)
